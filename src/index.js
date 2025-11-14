d644eb673d6a1c0545bf28f19f3804114b456db5
Ashish Tomar <84125340+tashish419@users.noreply.github.com>
2025-11-14 07:24:51 +0000

    connect to mongo atlas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+import dotenv from 'dotenv'
 
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from './db/index.js';
+
+dotenv.config({
+    path: "./.env"
+})
+
+connectDB()
 
 /*
 import express from "express";
