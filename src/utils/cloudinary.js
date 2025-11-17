d89beba8613ae2d7f4da36ac2460ac85206a778f
Ashish Tomar <84125340+tashish419@users.noreply.github.com>
2025-11-17 08:45:48 +0000

    configured coudinary & multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,21 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: proccess.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async function (localpathUrl) {
+    try {
+        if (!localpathUrl) return null;
+
+        await cloudinary.uploader.upload(localpathUrl, { resource_type: "auto" })
+    } catch (error) {
+        fs.unlinkSync(localpathUrl)
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
