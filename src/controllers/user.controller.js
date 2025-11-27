4bc3861fe15ad757e55a6f40f5ab654cc59f7e6f
Ashish Tomar <84125340+tashish419@users.noreply.github.com>
2025-11-27 09:24:52 +0000

    improve message

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -465,7 +465,7 @@ const getWatchHistory = asyncHandler(async (req, res) => {
     return res
         .status(200)
         .json(
-            new ApiResponse(200, user[0].watchHistory, "User details fetched successfully")
+            new ApiResponse(200, user[0].watchHistory, "Watch History fetched successfully")
         )
 })
 
